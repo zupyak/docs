1c1590566e648a4f880b1537400c2dc45900872b
milo <milo@iir.ac.cn>
2022-08-11 11:59:39 +0800

    relative path

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://swagger.iir.ac.cn/doc/zupyak.json",
+    url: "/doc/user.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
